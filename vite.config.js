f26bf40e01f79202b7719ed1e0bbc06c1cb5c1df
Rishabh567892 <146851398+Rishabh567892@users.noreply.github.com>
2025-11-14 10:42:08 +0000

    renamed:    public/sw.js -> public/service-worker.js 	modified:   src/main.js 	modified:   vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -17,8 +17,8 @@ export default defineConfig({
       closeBundle: async () => {
         // Inject Workbox manifest after Vite finishes building
         await injectManifest({
-          swSrc: "sw.js",      // your source file in project root
-          swDest: "dist/sw.js", // final SW in dist
+          swSrc: "service-worker.js",      // your source file in project root
+          swDest: "dist/service-worker.js", // final SW in dist
           globDirectory: "dist",
           globPatterns: ["**/*.{html,js,css,png,svg,ico,json}"],
         });
