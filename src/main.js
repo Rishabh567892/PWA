a9b8cea7f1d9ab1dedcea8726ced7ef967496aef
Rishabh567892 <146851398+Rishabh567892@users.noreply.github.com>
2025-11-15 07:07:25 +0000

    modified:   index.html 	renamed:    src/javascript.svg -> public/javascript.svg 	modified:   src/main.js

--- src/main.js
+++ src/main.js
@@ -1,5 +1,8 @@
+import './style.css'
+
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', () => {
     navigator.serviceWorker.register('/service-worker.js');
+    console.log("service-worker loaded")
   });
 }
